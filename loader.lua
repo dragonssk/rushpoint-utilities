faf7b766a1d95e8fc28b598deef04265b1e13eda
dragons <102265292+dragonssk@users.noreply.github.com>
2022-09-02 16:59:49 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/dragonssk/rushpoint-utilities/main/main.lua'))('github.com/dragonssk dragons#9500');
+loadstring(game:HttpGet('https://raw.githubusercontent.com/dragonssk/rushpoint-utilities/main/main.lua'))('github.com/dragonssk');
